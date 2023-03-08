807c3769f80d7db0899588d720266571b44c751f
duanxin <xin.duan@beantechs.com>
2023-03-08 15:09:38 +0800

    增加谷歌云地址

--- src/views/Subconverter.vue
+++ src/views/Subconverter.vue
@@ -349,6 +349,7 @@ export default {
         },
         backendOptions: [
           { value: "http://sub-backend.goldthree.top:25500/sub?" },
+          { value: "http://sub-gc.goldthree.top:25500/sub?" },
         ],
         remoteConfig: [
           {
